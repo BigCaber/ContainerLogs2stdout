a4a5a7aa7cc2a3444d57ebb6c6f1f315b1c2725d
BigCaber <rect@outlook.com>
2024-06-07 18:56:07 +0300

    Create tail-logs.sh

---
+++ tail-logs.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+# Create a list of log files
+log_files=$(find /var/log -type f -name "*.log")
+
+# Tail every file that ends with log, you can change above according to your needs.
+for FILE in $log_files; do
+  # Send all the contents of container outputs to docker stdout do it in the background and push outputs to pid1.
+  # Original idea: https://serverfault.com/a/932888 
+  tail -f $log_files & >> /proc/1/fd/1 
+done
+
+
+### Entrypoint stuff
+# Example:
+/etc/init.d/cron restart && apache2-foreground
+# Format:
+< your output of `sudo docker inspect -f '{{.Config.Entrypoint}}' <your_container_name>`>
